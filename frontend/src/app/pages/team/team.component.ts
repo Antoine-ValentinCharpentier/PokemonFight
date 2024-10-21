b057b7d4e254311beaf79dd679799b4c01882574
Antoine-ValentinCharpentier <antoinevalentin.charpentier@gmail.com>
2024-10-21 22:58:01 +0200

    :sparkles: Filter Pokédex

--- frontend/src/app/pages/team/team.component.ts
+++ frontend/src/app/pages/team/team.component.ts
@@ -2,27 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { TeamService } from '../../services/team.service';
 import { Pokemon } from '../../../type';
 import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [PokemonCardComponent],
+  imports: [PokemonCardComponent, CommonModule, FormsModule],
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss',
 })
 export class TeamComponent implements OnInit {
   constructor(private teamService: TeamService) {}
 
+  searchTermPokedex: string = '';
+
   pokedex: Pokemon[] = [];
+  filteredPokedex: Pokemon[] = [];
 
   ngOnInit() {
     this.teamService.getPokedex().subscribe({
       next: (data: Pokemon[]) => {
         this.pokedex = data;
+        this.filteredPokedex = data;
       },
       error: (error) => {
         console.log(error);
       },
     });
+  }
+
+  onSearchTermChange() {
+    if (this.searchTermPokedex) {
+      this.filteredPokedex = this.pokedex.filter((pokemon) =>
+        pokemon.name
+          .toLowerCase()
+          .includes(this.searchTermPokedex.toLowerCase())
+      );
+    } else {
+      this.filteredPokedex = this.pokedex;
+    }
   }
 }
